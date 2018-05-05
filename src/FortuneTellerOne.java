10383656a5e95529555f6f9b5936f47ca6e04ae3
Tai-Renee <TaiReneeK09@gmail.com>
2018-05-05 14:27:19 -0400

    Added src and bin

---
+++ src/FortuneTellerOne.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class FortuneTellerOne {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
